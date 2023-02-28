b5c7b7f16129826d7ed427913bbc391d36a8170d
gavinquach <gvqch8@gmail.com>
2023-02-28 16:49:15 +0700

    changed sky color

--- Chapter 3 - Advanced Techniques/22 - Imported models/src/script.js
+++ Chapter 3 - Advanced Techniques/22 - Imported models/src/script.js
@@ -12,7 +12,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('lightgreen');
+scene.background = new THREE.Color('skyblue');
 
 /**
  * Models
